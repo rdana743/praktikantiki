97f1e412588ad042bc39d08fda82900a82aafb1e
pashaly.artur <pashaly.artur@gmail.com>
2023-07-13 20:56:00 +0500

    for danil new version 13-07

--- djangotest/djangotest/djangotest/urls.py
+++ djangotest/djangotest/djangotest/urls.py
@@ -1,13 +1,17 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from main.views import index
 
 from django.conf import settings
 from django.conf.urls.static import static
+from main import views  
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('', index, name='index'),
     path('news/', include('news.urls')),
+    path('about', views.about, name='about'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
